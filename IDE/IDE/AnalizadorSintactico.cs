fc56592a9f9cb82187d99a9fa7788435d8b8e397
USUARIO <USUARIO@LAPTOP-BH0QBSA3>
2020-10-11 16:26:46 -0600

    Actualizacion

--- IDE/IDE/AnalizadorSintactico.cs
+++ IDE/IDE/AnalizadorSintactico.cs
@@ -1,14 +1,29 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using System.Windows.Forms;
 
 namespace IDE
 {
     class AnalizadorSintactico
     {
-        ArrayList tokens;
+        static private List<ErroresToken> listaErrores;
         Analizador lexico = new Analizador();
+       
+        public AnalizadorSintactico()
+        {
+           
+            //errores tokens
+            listaErrores = new List<ErroresToken>();
+
+        }
+        public void addError(String lexema, String idToken, int linea, int columna)
+        {
+            ErroresToken errtok = new ErroresToken(lexema, idToken, linea, columna);
+            listaErrores.Add(errtok);
+        }
+
 
         public void AnalizadorL(String[] datos)
         {
@@ -16,150 +31,123 @@ namespace IDE
             int total = datos.Length;
             int contador = 0;
             int produccion = 0;
-            List<Token> listaTokens;
             String resultado = "";
+            int columna = 1;
+            int fila = 1;
+            int estado = 0;
+            string lexema = "";
 
             for (int i = 0; i < datos.Length; i++)
             {
+                columna++;
                 Boolean tradu = false;
                 String valor = datos[i].ToString();
+         
 
-                if (!valor.Equals("Principal ()") && i == 0)
+                if (!valor.Equals("Principal ") && i == 0)
                 {
-                    errores += "Falta metodo inicial. Se esperaba Principal () \n";
+                    MessageBox.Show("Falta Metodo Principal", "Cuadro Errores");
                 }
-
-               else if (i == 0)
+                else if (i == 0)
                 {
                     produccion++;
-                    tradu = true;
+                    tradu = true; 
+                    MessageBox.Show("No se han encontrado Errores Semanticos", "Cuadro Errores");
                 }
-  
-                //validacion de variable 
-                if (valor.Contains("entero") || valor.Contains("cadena") || valor.Contains("booleano") || valor.Contains("decimal") || valor.Contains("caracter") && !valor.Contains("SI"))
-                {
-                    try
-                    {
-                        int inicioval = 0;
-                        int inicioval2 = fin(datos[i], "=");
-                        string ident = datos[i].Substring((inicioval), (inicioval2));
-                        if (lexico.Reservada(ident.Trim()))
-                        {
-                            errores += "Se esperaba un identificador \n";
 
+                    //validacion de variable 
+                    if (valor.Contains("entero") || valor.Contains("cadena") || valor.Contains("booleano") || valor.Contains("decimal") || valor.Contains("caracter") && !valor.Contains("SI") && !valor.Contains("SINO"))
+                        {
+                            try
+                            {
+                                int inicioval = 0;
+                                int inicioval2 = fin(datos[i], "=");
+                                string ident = datos[i].Substring((inicioval), (inicioval2));
+                                if (lexico.Reservada(ident.Trim()))
+                                {
+                                    MessageBox.Show( "Se esperaba un identificador \n");
+
+                                }
+
+                                string tipo = "";
+                                if (valor.Contains("entero"))
+                                {
+                                    tipo = "Int";
+                                }
+                                else if (valor.Contains("cadena"))
+                                {
+                                    tipo = "String";
+                                }
+                                else if (valor.Contains("booleano"))
+                                {
+                                    tipo = "Boolean";
+                                }
+                                else if (valor.Contains("decimal"))
+                                {
+                                    tipo = "Decimal";
+
+                                }
+                                else if (valor.Contains("Caracter"))
+                                {
+                                    tipo = "Char";
+                                }
+                                tradu = true;
                             }
 
-                        string tipo = "";
-                        if (valor.Contains("entero"))
-                        {
-                            tipo = "Int";
-                        }
-                        else if (valor.Contains("cadena"))
-                        {
-                            tipo = "String";
-                        }
-                        else if (valor.Contains("booleano"))
-                        {
-                            tipo = "Boolean";
-                        }
-                        else if (valor.Contains("decimal"))
-                        {
-                            tipo = "Decimal";
+                            catch (Exception e)
+                            {
+                                MessageBox.Show( "Error de gramatica de variable " + datos[i] + "\n");
+                            }
 
                         }
-                        else if (valor.Contains("Caracter"))
-                        {
-                            tipo = "Char";
-                        }
-                        tradu = true;
-                }
-
-                    catch (Exception e)
-                    {
-                        errores += "Error de gramatica de variable " + datos[i] + "\n";
-                    }
-
-                }
 
                 if (valor.Contains("SI"))
                 {
                     try
                     {
                         int inicioval = 0;
-                        int inicioval2 = fin(datos[i], "(");
-                        //nombre que se le dara al metodo 
+                        int inicioval2 = fin(datos[i], ")");
                         string ident = datos[i].Substring((inicioval), (inicioval2));
                         if (lexico.Reservada(ident.Trim()))
                         {
-                            errores += "Se esperaba Parentesis \n";
+                            MessageBox.Show("Se esperaba parentesis \n");
 
                         }
-                        
+
                         string llave = "";
-                        if (valor.Trim().Contains("{"))
-                        {
+                        if (valor.Trim().Contains("{")) {
                             llave = "{";
                         }
-
-                        if (valor.Trim().Contains("}"))
-                        {
-                            llave = "}";
-                        }
-
-
-                       else if (valor.Contains("SINO"))
-                        {
-                            inicioval = inicio(datos[i], "{");
-                            inicioval2 = fin(datos[i], "");
-                            //nombre que se le dara al metodo 
-                            ident = datos[i].Substring((inicioval), (inicioval2));
-                            if (lexico.Reservada(ident.Trim()))
-                            {
-                                errores += "Se esperaban llaves \n";
-
-                            }
-
-                        }
-                        
 
                     }
                     catch (Exception e)
                     {
-                        errores += "Error en la gramatica de la funcion ingresada ";
+
+                        MessageBox.Show("Se esperaba Parentesis " + datos[i] + "\n");
+                    }
+                    
                     }
 
                 }
 
+                
 
-
-            }
-
-
-
+                
 
 
 
-
-
-
-            if (errores != "")
-                {
-                    MessageBox.Show(errores, "Cuadro Errores");
-
-                }
-                else
-                {
-                    MessageBox.Show("No se han encontrado Errores Semanticos", "Cuadro Errores");
-                }
-
-
-
+            
 
 
 
             }
-        
 
+
+
+       
+
+
+                
         private int inicio(string cadena, string comparacion)
         {
             int inicio = cadena.ToString().IndexOf(comparacion.ToUpper());
@@ -179,6 +167,6 @@ namespace IDE
             return fin;
         }
 
-
-        }
+  
+    }
     }
---
+++ IDE/IDE/ErroresSintacticos.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDE
+{
+    class ErroresSintacticos
+    {
+     
+        public int num_lineaE;
+        public string error;
+        public string descrip;
+        public string sol_error;
+        public int columna;
+
+        public ErroresSintacticos( int nl, string e, string se, int columna )
+        {
+            
+
+                num_lineaE = nl;
+                error = e;
+                sol_error = se;
+                columna = columna;
+
+            }
+            
+
+            public ErroresSintacticos ()
+            {
+
+            }
+
+
+
+        public int NumerodeLinea
+        {
+            get { return num_lineaE; }
+            set { num_lineaE = value; }
+        }
+
+        public string Error
+        {
+            get { return error; }
+            set { error = value; }
+        }
+
+        public string Solucion
+        {
+            get { return sol_error; }
+            set { sol_error = value; }
+        }
+        public string Descripcion
+        {
+            get { return descrip; }
+            set { descrip = value; }
+        }
+
+        public int Columna
+        {
+            get { return columna; }
+            set { columna = value; }
+        }
+
+    }
+    }
